65759dcde7c38d5755ecb53a46c997ddf6d5dd5f
권도현 <tobeykwon@gmail.com>
2024-06-23 17:02:27 +0900

    refact: MemberRequest 리터럴 제거

--- back/src/main/java/com/kwon/crmproject/member/dto/MemberRequest.java
+++ back/src/main/java/com/kwon/crmproject/member/dto/MemberRequest.java
@@ -2,30 +2,57 @@ package com.kwon.crmproject.member.dto;
 
 import com.kwon.crmproject.common.constant.ValidationProperties;
 import jakarta.validation.constraints.Pattern;
+import lombok.Data;
 import org.hibernate.validator.constraints.Length;
 
 public abstract class MemberRequest {
+
+    @Data
     public static class Join {
-        @Length(max = ValidationProperties.STRING_MAX_LENGTH, message = ValidationProperties.Message.STRING_MAX_LENGTH)
+        @Length(
+                max = ValidationProperties.STRING_MAX_LENGTH,
+                message = ValidationProperties.Message.STRING_MAX_LENGTH
+        )
         private String companyName;
 
-        @Length(max = ValidationProperties.STRING_MAX_LENGTH, message = "{String.length.max}")
+        @Length(
+                max = ValidationProperties.STRING_MAX_LENGTH,
+                message = ValidationProperties.Message.STRING_MAX_LENGTH
+        )
         private String username;
 
-        @Length(max = ValidationProperties.STRING_MAX_LENGTH, message = "{String.length.max}")
+        @Length(
+                max = ValidationProperties.STRING_MAX_LENGTH,
+                message = ValidationProperties.Message.STRING_MAX_LENGTH
+        )
         private String password;
 
-        @Length(max = ValidationProperties.STRING_MAX_LENGTH, message = "{String.length.max}")
+        @Length(
+                max = ValidationProperties.STRING_MAX_LENGTH,
+                message = ValidationProperties.Message.STRING_MAX_LENGTH
+        )
         private String businessRegistrationNumber;
 
-        @Length(max = ValidationProperties.STRING_MAX_LENGTH, message = "{String.length.max}")
+        @Length(
+                max = ValidationProperties.STRING_MAX_LENGTH,
+                message = ValidationProperties.Message.STRING_MAX_LENGTH
+        )
         private String email;
 
-        @Length(max = ValidationProperties.STRING_MAX_LENGTH, message = "{String.length.max}")
-        @Pattern(regexp = ValidationProperties.EXIST_HYPHEN_CHECK_PATTERN, message = "{validation.field.contact}")
+        @Length(
+                max = ValidationProperties.STRING_MAX_LENGTH,
+                message = ValidationProperties.Message.STRING_MAX_LENGTH
+        )
+        @Pattern(
+                regexp = ValidationProperties.EXIST_HYPHEN_CHECK_PATTERN,
+                message = ValidationProperties.Message.CONTACT_FIELD_VALIDATION
+        )
         private String contact;
 
-        @Length(max = ValidationProperties.STRING_MAX_LENGTH, message = "{String.length.max}")
+        @Length(
+                max = ValidationProperties.STRING_MAX_LENGTH,
+                message = ValidationProperties.Message.STRING_MAX_LENGTH
+        )
         private String accountHolder;
     }
 }
