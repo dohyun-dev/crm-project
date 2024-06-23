6b71f359fc7be93b2b3be981d70304536bcfbe0d
권도현 <tobeykwon@gmail.com>
2024-06-23 15:19:17 +0900

    feat: Member 엔티티 추가

---
+++ back/src/main/java/com/kwon/crmproject/member/domain/entity/Member.java
@@ -0,0 +1,38 @@
+package com.kwon.crmproject.member.domain.entity;
+
+import com.kwon.crmproject.common.entity.BaseTimeEntity;
+import jakarta.persistence.Entity;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class Member extends BaseTimeEntity {
+
+    private String companyName;
+    private String username;
+    private String password;
+    private String businessRegistrationNumber;
+    private String email;
+    private String contact;
+    private String accountHolder;
+
+    @Builder
+    public Member(
+            Long id, String companyName, String username,
+            String password, String businessRegistrationNumber,
+            String email, String contact, String accountHolder
+    ) {
+        super(id);
+        this.companyName = companyName;
+        this.username = username;
+        this.password = password;
+        this.businessRegistrationNumber = businessRegistrationNumber;
+        this.email = email;
+        this.contact = contact;
+        this.accountHolder = accountHolder;
+    }
+}
