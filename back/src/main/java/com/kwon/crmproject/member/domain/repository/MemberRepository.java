979eda3cbf92b53a5e3e3908dc2260fefc06ba35
권도현 <tobeykwon@gmail.com>
2024-06-24 11:19:48 +0900

    feat: MemberRepository existsByUsername 메서드 추가

--- back/src/main/java/com/kwon/crmproject/member/domain/repository/MemberRepository.java
+++ back/src/main/java/com/kwon/crmproject/member/domain/repository/MemberRepository.java
@@ -4,4 +4,5 @@ import com.kwon.crmproject.member.domain.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    boolean existsByUsername(String username);
 }
