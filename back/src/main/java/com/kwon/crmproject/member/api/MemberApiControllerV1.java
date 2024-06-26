f315238d09bccfe8ef24245bc22f23a8a8ca6a5a
권도현 <tobeykwon@gmail.com>
2024-06-26 14:21:56 +0900

    feat: MemberApiControllerV1 추가

---
+++ back/src/main/java/com/kwon/crmproject/member/api/MemberApiControllerV1.java
@@ -0,0 +1,48 @@
+package com.kwon.crmproject.member.api;
+
+import com.kwon.crmproject.member.domain.entity.Member;
+import com.kwon.crmproject.member.dto.MemberRequest;
+import com.kwon.crmproject.member.service.MemberServiceV1;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/members")
+public class MemberApiControllerV1 {
+
+    private final MemberServiceV1 memberService;
+
+    @GetMapping("/{memberId}")
+    public Member findDetail(@PathVariable("memberId") Long memberId) {
+        return memberService.findDetail(memberId);
+    }
+
+    @GetMapping
+    public Page<Member> findAll(MemberRequest.SearchCondition searchCondition, Pageable pageable) {
+        return memberService.findAll(searchCondition, pageable);
+    }
+
+    @PostMapping
+    public void create(@Valid @RequestBody MemberRequest.Command request) {
+        memberService.create(request);
+    }
+
+    @PutMapping("/{memberId}")
+    public void edit(
+            @PathVariable("memberId") Long memberId,
+            @Valid @RequestBody MemberRequest.Command request
+    ) {
+        memberService.edit(memberId, request);
+    }
+
+    @DeleteMapping("/{memberId}")
+    public void delete(
+            @PathVariable("memberId") Long memberId
+    ) {
+        memberService.delete(memberId);
+    }
+}
