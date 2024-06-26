597a369b85ed376bf8fdc2ea6352dc4a78bddc8e
권도현 <tobeykwon@gmail.com>
2024-06-26 23:25:55 +0900

    feat: Member API 추가

---
+++ front/src/apis/member.js
@@ -0,0 +1,15 @@
+import axiosInstance from './axios';
+import API_URLS from '../constants/urls';
+
+const createMember = async (data) => {
+  const response = await axiosInstance.post(API_URLS.MEMBER.CREATE_URL(), data);
+  return response.data;
+};
+
+const API = {
+  MEMBER: {
+    createMember,
+  },
+};
+
+export default API;
