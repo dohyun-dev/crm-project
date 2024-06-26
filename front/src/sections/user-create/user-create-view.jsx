9fe89c9f345e080a89122151ad4c0e27406c78f3
권도현 <tobeykwon@gmail.com>
2024-06-26 23:27:41 +0900

    feat: user-create-view.jsx API 연결 및 밸리데이션 추가

--- front/src/sections/user-create/user-create-view.jsx
+++ front/src/sections/user-create/user-create-view.jsx
@@ -1,4 +1,9 @@
+import React from 'react';
+import * as yup from 'yup';
+import Swal from 'sweetalert2';
 import { useNavigate } from 'react-router-dom';
+import { useForm, Controller } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
 
 import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
@@ -8,12 +13,102 @@ import Divider from '@mui/material/Divider';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import CardHeader from '@mui/material/CardHeader';
-import { InputLabel, FormControl, CardActions, CardContent, OutlinedInput } from '@mui/material';
+import {
+  InputLabel,
+  CardActions,
+  CardContent,
+  FormControl,
+  OutlinedInput,
+  FormHelperText,
+} from '@mui/material';
 
+import API from '../../apis/member';
 import Iconify from '../../components/iconify';
+
+const STRING_MAX_LENGTH = 1024;
+const EXIST_HYPHEN_CHECK_PATTERN = /^[0-9]+$/;
+
+const validationSchema = yup.object().shape({
+  companyName: yup
+    .string()
+    .max(STRING_MAX_LENGTH, `업체명은 최대 ${STRING_MAX_LENGTH}자까지 입력 가능합니다.`)
+    .required('업체명을 입력해주세요.'),
+  username: yup
+    .string()
+    .max(STRING_MAX_LENGTH, `아이디는 최대 ${STRING_MAX_LENGTH}자까지 입력 가능합니다.`)
+    .required('아이디를 입력해주세요.'),
+  password: yup
+    .string()
+    .max(STRING_MAX_LENGTH, `비밀번호는 최대 ${STRING_MAX_LENGTH}자까지 입력 가능합니다.`)
+    .required('비밀번호를 입력해주세요.'),
+  businessRegistrationNumber: yup
+    .string()
+    .max(STRING_MAX_LENGTH, `사업자등록번호는 최대 ${STRING_MAX_LENGTH}자까지 입력 가능합니다.`)
+    .required('사업자등록번호를 입력해주세요.'),
+  email: yup
+    .string()
+    .max(STRING_MAX_LENGTH, `이메일은 최대 ${STRING_MAX_LENGTH}자까지 입력 가능합니다.`)
+    .email('유효한 이메일을 입력해주세요.')
+    .required('이메일을 입력해주세요.'),
+  contact: yup
+    .string()
+    .matches(EXIST_HYPHEN_CHECK_PATTERN, '연락처는 숫자만 입력할 수 있습니다.')
+    .max(STRING_MAX_LENGTH, `연락처는 최대 ${STRING_MAX_LENGTH}자까지 입력 가능합니다.`)
+    .required('연락처를 입력해주세요.'),
+  accountHolder: yup
+    .string()
+    .max(STRING_MAX_LENGTH, `예금주는 최대 ${STRING_MAX_LENGTH}자까지 입력 가능합니다.`)
+    .required('예금주를 입력해주세요.'),
+});
 
 export default function UserCreateView() {
   const navigate = useNavigate();
+
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+    setError,
+  } = useForm({
+    mode: 'onChange',
+    resolver: yupResolver(validationSchema),
+  });
+
+  const onSubmit = (data) => {
+    API.MEMBER.createMember(data)
+      .then((response) => {
+        Swal.fire({
+          title: '성공!',
+          text: '회원 생성에 성공했습니다.',
+          icon: 'success',
+          confirmButtonText: '확인',
+        });
+        navigate('/user');
+      })
+      .catch((error) => {
+        switch (error.response.data.type) {
+          case 'ERROR': {
+            Swal.fire({
+              title: '실패!',
+              text: error.response.data.description,
+              icon: 'error',
+              confirmButtonText: '확인',
+            });
+            break;
+          }
+          default: {
+            const errorDetails = error.response.data.errors;
+            Object.keys(errorDetails).forEach((field) => {
+              setError(field, {
+                type: 'field',
+                message: errorDetails[field],
+              });
+            });
+            break;
+          }
+        }
+      });
+  };
 
   return (
     <Container maxWidth="lg" sx={{ display: 'flex', flexDirection: 'column' }}>
@@ -34,7 +129,7 @@ export default function UserCreateView() {
       </Stack>
       {/* 콘텐츠 헤더 끝 */}
 
-      <form noValidate autoComplete="off">
+      <form noValidate autoComplete="off" onSubmit={handleSubmit(onSubmit)}>
         <Stack spacing={2}>
           <Card sx={{ p: 1 }}>
             <CardHeader
@@ -47,46 +142,107 @@ export default function UserCreateView() {
 
             <CardContent>
               <Stack p={1} spacing={2}>
-                <Typography variant="subtitle2">업체명</Typography>
                 <FormControl fullWidth variant="outlined" size="small">
                   <InputLabel htmlFor="company">업체명</InputLabel>
-                  <OutlinedInput id="company" label="업체명" />
+                  <Controller
+                    name="companyName"
+                    control={control}
+                    defaultValue=""
+                    render={({ field }) => <OutlinedInput {...field} id="company" label="업체명" />}
+                  />
+                  {errors.companyName && (
+                    <FormHelperText error>{errors.companyName.message}</FormHelperText>
+                  )}
                 </FormControl>
 
-                <Typography variant="subtitle2">아이디</Typography>
                 <FormControl fullWidth variant="outlined" size="small">
                   <InputLabel htmlFor="username">아이디</InputLabel>
-                  <OutlinedInput id="username" label="아이디" />
+                  <Controller
+                    name="username"
+                    control={control}
+                    defaultValue=""
+                    render={({ field }) => (
+                      <OutlinedInput {...field} id="username" label="아이디" />
+                    )}
+                  />
+                  {errors.username && (
+                    <FormHelperText error>{errors.username.message}</FormHelperText>
+                  )}
                 </FormControl>
 
-                <Typography variant="subtitle2">비밀번호</Typography>
                 <FormControl fullWidth variant="outlined" size="small">
                   <InputLabel htmlFor="password">비밀번호</InputLabel>
-                  <OutlinedInput id="password" label="비밀번호" />
+                  <Controller
+                    name="password"
+                    control={control}
+                    defaultValue=""
+                    render={({ field }) => (
+                      <OutlinedInput {...field} id="password" label="비밀번호" />
+                    )}
+                  />
+                  {errors.password && (
+                    <FormHelperText error>{errors.password.message}</FormHelperText>
+                  )}
                 </FormControl>
 
-                <Typography variant="subtitle2">사업자등록번호</Typography>
                 <FormControl fullWidth variant="outlined" size="small">
                   <InputLabel htmlFor="businessRegistrationNumber">사업자등록번호</InputLabel>
-                  <OutlinedInput id="businessRegistrationNumber" label="사업자등록번호" />
+                  <Controller
+                    name="businessRegistrationNumber"
+                    control={control}
+                    defaultValue=""
+                    render={({ field }) => (
+                      <OutlinedInput
+                        {...field}
+                        id="businessRegistrationNumber"
+                        label="사업자등록번호"
+                      />
+                    )}
+                  />
+                  {errors.businessRegistrationNumber && (
+                    <FormHelperText error>
+                      {errors.businessRegistrationNumber.message}
+                    </FormHelperText>
+                  )}
                 </FormControl>
 
-                <Typography variant="subtitle2">이메일</Typography>
                 <FormControl fullWidth variant="outlined" size="small">
                   <InputLabel htmlFor="email">이메일</InputLabel>
-                  <OutlinedInput id="email" label="이메일" />
+                  <Controller
+                    name="email"
+                    control={control}
+                    defaultValue=""
+                    render={({ field }) => <OutlinedInput {...field} id="email" label="이메일" />}
+                  />
+                  {errors.email && <FormHelperText error>{errors.email.message}</FormHelperText>}
                 </FormControl>
 
-                <Typography variant="subtitle2">연락처</Typography>
                 <FormControl fullWidth variant="outlined" size="small">
                   <InputLabel htmlFor="contact">연락처</InputLabel>
-                  <OutlinedInput id="contact" label="연락처" />
+                  <Controller
+                    name="contact"
+                    control={control}
+                    defaultValue=""
+                    render={({ field }) => <OutlinedInput {...field} id="contact" label="연락처" />}
+                  />
+                  {errors.contact && (
+                    <FormHelperText error>{errors.contact.message}</FormHelperText>
+                  )}
                 </FormControl>
 
-                <Typography variant="subtitle2">예금주</Typography>
                 <FormControl fullWidth variant="outlined" size="small">
                   <InputLabel htmlFor="accountHolder">예금주</InputLabel>
-                  <OutlinedInput id="accountHolder" label="예금주" />
+                  <Controller
+                    name="accountHolder"
+                    control={control}
+                    defaultValue=""
+                    render={({ field }) => (
+                      <OutlinedInput {...field} id="accountHolder" label="예금주" />
+                    )}
+                  />
+                  {errors.accountHolder && (
+                    <FormHelperText error>{errors.accountHolder.message}</FormHelperText>
+                  )}
                 </FormControl>
               </Stack>
             </CardContent>
@@ -98,6 +254,7 @@ export default function UserCreateView() {
                 variant="contained"
                 color="primary"
                 startIcon={<Iconify icon="eva:checkmark-circle-2-outline" />}
+                type="submit"
                 fullWidth
                 sx={{ py: 1 }}
               >
