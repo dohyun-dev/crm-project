62e2cdb15492ec8bcec2560214c33d0042fa5362
권도현 <tobeykwon@gmail.com>
2024-06-26 16:08:36 +0900

    feat: EnableJpaAuditing 추가

--- back/src/main/java/com/kwon/crmproject/CrmProjectApplication.java
+++ back/src/main/java/com/kwon/crmproject/CrmProjectApplication.java
@@ -2,7 +2,9 @@ package com.kwon.crmproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class CrmProjectApplication {
 
--- back/src/main/java/com/kwon/crmproject/common/entity/BaseEntity.java
+++ back/src/main/java/com/kwon/crmproject/common/entity/BaseEntity.java
@@ -7,11 +7,9 @@ import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import java.time.LocalDateTime;
 
-@EnableJpaAuditing
 @EntityListeners(AuditingEntityListener.class)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @MappedSuperclass
