808065773b4720b04fdcb5544ccd52558b5bab5d
권도현 <tobeykwon@gmail.com>
2024-06-26 14:21:44 +0900

    feat: ServiceImpl 추가

---
+++ back/src/main/java/com/kwon/crmproject/campaign/service/impl/CampaignServiceV1Impl.java
@@ -0,0 +1,119 @@
+package com.kwon.crmproject.campaign.service.impl;
+
+import com.kwon.crmproject.campaign.domain.entity.Campaign;
+import com.kwon.crmproject.campaign.domain.entity.CampaignState;
+import com.kwon.crmproject.campaign.domain.repository.CampaignRepository;
+import com.kwon.crmproject.campaign.dto.CampaignRequest;
+import com.kwon.crmproject.campaign.service.CampaignServiceV1;
+import com.kwon.crmproject.common.exception.CustomException;
+import com.kwon.crmproject.common.exception.ErrorType;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class CampaignServiceV1Impl implements CampaignServiceV1 {
+
+    public static final int VALIDATION_HOUR_THRESHOLD = 16;
+
+    private final CampaignRepository campaignRepository;
+
+    @Override
+    public Campaign findDetail(Long campaignId) {
+        return getCampaign(campaignId);
+    }
+
+    @Transactional
+    @Override
+    public Page<Campaign> findAll(CampaignRequest.SearchCondition searchCondition, Pageable pageable) {
+        Campaign exampleCampaign = Campaign.builder()
+                .keyword(searchCondition.keyword())
+                .companyName(searchCondition.companyName())
+                .rewardType(searchCondition.rewardType())
+                .build();
+
+        Example<Campaign> example = Example.of(exampleCampaign);
+
+        return campaignRepository.findAll(example, pageable);
+    }
+
+    @Transactional
+    @Override
+    public void create(CampaignRequest.Create request) {
+        validateStartDateAgainstCurrentTime(request.getStartDate());
+
+        LocalDate endDate = getEndDate(request.getStartDate(), request.getPeriod());
+
+        Campaign campaign = Campaign.builder()
+                .keyword(request.getKeyword())
+                .companyName(request.getCompanyName())
+                .url(request.getUrl())
+                .mid(request.getMid())
+                .startDate(request.getStartDate())
+                .endDate(endDate)
+                .rewardType(request.getRewardType())
+                .trafficRequest(Integer.parseInt(request.getTrafficRequest()))
+                .state(CampaignState.PENDING_APPROVAL)
+                .build();
+
+        campaignRepository.save(campaign);
+    }
+
+    @Override
+    public void edit(Long campaignId, CampaignRequest.Edit request) {
+        Campaign findCampaign = getCampaign(campaignId);
+        LocalDate endDate = getEndDate(request.getStartDate(), request.getPeriod());
+
+        findCampaign.update(
+                request.getKeyword(),
+                request.getCompanyName(),
+                request.getUrl(),
+                request.getMid(),
+                request.getStartDate(),
+                endDate,
+                request.getRewardType(),
+                Integer.parseInt(request.getTrafficRequest()),
+                request.getCampaignState()
+        );
+    }
+
+    @Override
+    public void delete(Long campaignId) {
+        Campaign findCampaign = getCampaign(campaignId);
+
+        campaignRepository.delete(findCampaign);
+    }
+
+    private Campaign getCampaign(Long campaignId) {
+        return campaignRepository.findById(campaignId)
+                .orElseThrow(() -> CustomException.of(ErrorType.CAMPAIGN_NOT_FOUND));
+    }
+
+    private LocalDate getEndDate(LocalDate startDate, int period) {
+        return startDate.plusDays(period);
+    }
+
+    private void validateStartDateAgainstCurrentTime(LocalDate startDate) {
+        LocalDateTime now = LocalDateTime.now();
+        LocalDate nowDate = now.toLocalDate();
+        int curHour = now.getHour();
+
+        LocalDate validDate = curHour < VALIDATION_HOUR_THRESHOLD ? nowDate.plusDays(0) : nowDate.plusDays(1);
+
+        if (!startDate.isAfter(validDate)) {
+            if (curHour < VALIDATION_HOUR_THRESHOLD) {
+                throw CustomException.of(ErrorType.CAMPAIGN_START_DATE_INVALID_BEFORE_16);
+            } else {
+                throw CustomException.of(ErrorType.CAMPAIGN_START_DATE_INVALID_AFTER_16);
+            }
+        }
+    }
+}
---
+++ back/src/main/java/com/kwon/crmproject/member/service/impl/MemberServiceV1Impl.java
@@ -0,0 +1,97 @@
+package com.kwon.crmproject.member.service.impl;
+
+import com.kwon.crmproject.common.exception.CustomException;
+import com.kwon.crmproject.common.exception.ErrorType;
+import com.kwon.crmproject.member.domain.entity.Member;
+import com.kwon.crmproject.member.domain.repository.MemberRepository;
+import com.kwon.crmproject.member.dto.MemberRequest;
+import com.kwon.crmproject.member.service.MemberServiceV1;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class MemberServiceV1Impl implements MemberServiceV1 {
+
+    private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public Member findDetail(Long memberId) {
+        return getMember(memberId);
+    }
+
+    @Override
+    public Page<Member> findAll(MemberRequest.SearchCondition searchCondition, Pageable pageable) {
+        Member exampleMember = Member.builder()
+                .companyName(searchCondition.companyName())
+                .build();
+
+        Example<Member> example = Example.of(exampleMember);
+
+        return memberRepository.findAll(example, pageable);
+    }
+
+    @Transactional
+    @Override
+    public void create(MemberRequest.Command request) {
+        validateParam(request);
+
+        Member newMember = Member.builder()
+                .companyName(request.getCompanyName())
+                .username(request.getUsername())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .businessRegistrationNumber(request.getBusinessRegistrationNumber())
+                .email(request.getEmail())
+                .contact(request.getContact())
+                .accountHolder(request.getAccountHolder())
+                .build();
+
+        memberRepository.save(newMember);
+    }
+
+    @Override
+    public void edit(Long memberId, MemberRequest.Command request) {
+        Member findMember = getMember(memberId);
+
+        String encodedPassword = getCurrentOrNewEncodedPassword(request.getPassword(), findMember.getPassword());
+
+        findMember.update(
+                request.getCompanyName(),
+                request.getUsername(),
+                encodedPassword,
+                request.getBusinessRegistrationNumber(),
+                request.getEmail(),
+                request.getContact(),
+                request.getAccountHolder()
+        );
+    }
+    @Override
+    public void delete(Long memberId) {
+        Member findMember = getMember(memberId);
+
+        memberRepository.delete(findMember);
+    }
+
+    private Member getMember(Long memberId) {
+        return memberRepository.findById(memberId)
+                .orElseThrow(() -> CustomException.of(ErrorType.MEMBER_NOT_FOUND));
+    }
+
+    private String getCurrentOrNewEncodedPassword(String inputPassword, String curPassword) {
+        if (passwordEncoder.matches(inputPassword, curPassword)) {
+            return curPassword;
+        }
+        return passwordEncoder.encode(inputPassword);
+    }
+
+    private void validateParam(MemberRequest.Command request) {
+        if (memberRepository.existsByUsername(request.getUsername()))
+            throw CustomException.of(ErrorType.USERNAME_DUPLICATED);
+    }
+}
