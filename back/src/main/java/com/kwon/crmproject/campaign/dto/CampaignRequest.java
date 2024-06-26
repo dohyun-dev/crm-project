a9cbae007f5b9cfecaa493d548c169f911a7f236
권도현 <tobeykwon@gmail.com>
2024-06-26 14:21:10 +0900

    feat: CampaignServiceV1 추가

--- back/src/main/java/com/kwon/crmproject/campaign/dto/CampaignRequest.java
+++ back/src/main/java/com/kwon/crmproject/campaign/dto/CampaignRequest.java
@@ -1,6 +1,7 @@
 package com.kwon.crmproject.campaign.dto;
 
 import com.kwon.crmproject.campaign.domain.entity.CampaignRewardType;
+import com.kwon.crmproject.campaign.domain.entity.CampaignState;
 import com.kwon.crmproject.common.constant.ValidationProperties;
 import com.kwon.crmproject.common.validator.annotation.MultipleOfFifty;
 import jakarta.validation.constraints.Pattern;
@@ -10,6 +11,14 @@ import org.hibernate.validator.constraints.Length;
 import java.time.LocalDate;
 
 public abstract class CampaignRequest {
+
+    public record SearchCondition(
+            CampaignRewardType rewardType,
+            String keyword,
+            String companyName
+    ) {
+
+    }
 
     @Data
     public static class Create {
@@ -69,5 +78,63 @@ public abstract class CampaignRequest {
         )
         @MultipleOfFifty
         private String trafficRequest;
+    }
+
+    @Data
+    public static class Edit {
+        @Length(
+                max = ValidationProperties.STRING_MAX_LENGTH,
+                message = ValidationProperties.Message.STRING_MAX_LENGTH
+        )
+        private String keyword;
+
+        @Length(
+                max = ValidationProperties.STRING_MAX_LENGTH,
+                message = ValidationProperties.Message.STRING_MAX_LENGTH
+        )
+        private String companyName;
+
+        @Length(
+                max = ValidationProperties.STRING_MAX_LENGTH,
+                message = ValidationProperties.Message.STRING_MAX_LENGTH
+        )
+        private String url;
+
+        @Length(
+                max = ValidationProperties.STRING_MAX_LENGTH,
+                message = ValidationProperties.Message.STRING_MAX_LENGTH
+        )
+        @Pattern(
+                regexp = ValidationProperties.IS_NUMBER_CHECK_PATTERN,
+                message = ValidationProperties.Message.IS_NUMBER_CHECK
+        )
+        private String mid;
+
+        @Length(
+                max = ValidationProperties.STRING_MAX_LENGTH,
+                message = ValidationProperties.Message.STRING_MAX_LENGTH
+        )
+        private LocalDate startDate;
+
+        @Length(
+                max = ValidationProperties.STRING_MAX_LENGTH,
+                message = ValidationProperties.Message.STRING_MAX_LENGTH
+        )
+        private int period;
+
+        private CampaignRewardType rewardType;
+
+        @Length(
+                max = ValidationProperties.STRING_MAX_LENGTH,
+                message = ValidationProperties.Message.STRING_MAX_LENGTH
+        )
+        @Pattern(
+                regexp = ValidationProperties.IS_NUMBER_CHECK_PATTERN,
+                message = ValidationProperties.Message.IS_NUMBER_CHECK
+        )
+        @MultipleOfFifty
+        private String trafficRequest;
+
+        private CampaignState campaignState;
     }
 }
---
+++ back/src/main/java/com/kwon/crmproject/campaign/service/CampaignServiceV1.java
@@ -0,0 +1,14 @@
+package com.kwon.crmproject.campaign.service;
+
+import com.kwon.crmproject.campaign.domain.entity.Campaign;
+import com.kwon.crmproject.campaign.dto.CampaignRequest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface CampaignServiceV1 {
+    Campaign findDetail(Long campaignId);
+    Page<Campaign> findAll(CampaignRequest.SearchCondition searchCondition, Pageable pageable);
+    void create(CampaignRequest.Create request);
+    void edit(Long campaignId, CampaignRequest.Edit request);
+    void delete(Long campaignId);
+}
