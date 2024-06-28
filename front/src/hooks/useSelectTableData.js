26cb447a319791293fa83726d2a026408fb54ed8
권도현 <tobeykwon@gmail.com>
2024-06-28 23:08:44 +0900

    feat: 테이블 데이터 선택 커스텀 훅 추가

---
+++ front/src/hooks/useSelectTableData.js
@@ -0,0 +1,40 @@
+import { useState } from 'react';
+
+const useSelectableData = (initialSelected = []) => {
+  const [selected, setSelected] = useState(initialSelected);
+
+  const handleClickAllTableRow = (event, data) => {
+    if (event.target.checked) {
+      const newSelecteds = data.map((n) => n.id);
+      setSelected(newSelecteds);
+      return;
+    }
+    setSelected([]);
+  };
+
+  const handleClickTableRow = (event, id) => {
+    const selectedIndex = selected.indexOf(id);
+    let newSelected = [];
+    if (selectedIndex === -1) {
+      newSelected = newSelected.concat(selected, id);
+    } else if (selectedIndex === 0) {
+      newSelected = newSelected.concat(selected.slice(1));
+    } else if (selectedIndex === selected.length - 1) {
+      newSelected = newSelected.concat(selected.slice(0, -1));
+    } else if (selectedIndex > 0) {
+      newSelected = newSelected.concat(
+        selected.slice(0, selectedIndex),
+        selected.slice(selectedIndex + 1)
+      );
+    }
+    setSelected(newSelected);
+  };
+
+  return {
+    selected,
+    handleClickAllTableRow,
+    handleClickTableRow,
+  };
+};
+
+export default useSelectableData;
