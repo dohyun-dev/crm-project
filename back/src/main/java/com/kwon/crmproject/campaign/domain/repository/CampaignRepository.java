cf52d1ec65cdfafcd92a91973b5dca0db7e4daab
권도현 <tobeykwon@gmail.com>
2024-07-03 20:38:47 +0900

    feat: CampaignRepository findByIdIn 추가

--- back/src/main/java/com/kwon/crmproject/campaign/domain/repository/CampaignRepository.java
+++ back/src/main/java/com/kwon/crmproject/campaign/domain/repository/CampaignRepository.java
@@ -3,5 +3,8 @@ package com.kwon.crmproject.campaign.domain.repository;
 import com.kwon.crmproject.campaign.domain.entity.Campaign;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CampaignRepository extends JpaRepository<Campaign, Long> {
+    List<Campaign> findByIdIn(List<Long> ids);
 }
