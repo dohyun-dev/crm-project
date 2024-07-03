0ccba054472cec8aed95cdbad758be3768911e38
권도현 <tobeykwon@gmail.com>
2024-07-03 20:38:29 +0900

    feat: CampaignServiceV1 상태 변경 및 연장 메서드 추가

--- back/src/main/java/com/kwon/crmproject/campaign/service/CampaignServiceV1.java
+++ back/src/main/java/com/kwon/crmproject/campaign/service/CampaignServiceV1.java
@@ -1,15 +1,19 @@
 package com.kwon.crmproject.campaign.service;
 
 import com.kwon.crmproject.campaign.domain.entity.Campaign;
+import com.kwon.crmproject.campaign.domain.entity.CampaignState;
 import com.kwon.crmproject.campaign.dto.CampaignRequest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+
+import java.util.List;
 
 public interface CampaignServiceV1 {
     Campaign findDetail(Long campaignId);
     Page<Campaign> findAll(CampaignRequest.SearchCondition searchCondition, Pageable pageable);
     void create(CampaignRequest.Create request);
     void edit(Long campaignId, CampaignRequest.Edit request);
-    void extendEndDate(CampaignRequest.ExtendEndDate extendEndDate);
+    void changeState(List<Long> campaignIds, CampaignState state);
+    void extendEndDate(List<Long> campaignIds, int extendDays);
     void delete(Long campaignId);
 }
