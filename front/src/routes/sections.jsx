2da01920579d59ac3c194b6246b56b4b2dd4d7da
권도현 <tobeykwon@gmail.com>
2024-07-06 16:29:48 +0900

    feat: 라우터 로그인 검증 추가

--- front/src/routes/sections.jsx
+++ front/src/routes/sections.jsx
@@ -1,4 +1,6 @@
-import { lazy, Suspense } from 'react';
+import Swal from 'sweetalert2';
+import { useRecoilState } from 'recoil';
+import { lazy, Suspense, useEffect } from 'react';
 import { Outlet, Navigate, useRoutes } from 'react-router-dom';
 
 import CampaignPage from 'src/pages/campaign';
@@ -8,34 +10,56 @@ import DashboardLayout from 'src/layouts/dashboard';
 import CampaignEditPage from 'src/pages/campaign-edit';
 import CampaignCreatePage from 'src/pages/campaign-create';
 
+import { authState } from '../recoil/atoms';
+
 export const UserPage = lazy(() => import('src/pages/user'));
 export const LoginPage = lazy(() => import('src/pages/login'));
 export const Page404 = lazy(() => import('src/pages/page-not-found'));
 
 // ----------------------------------------------------------------------
+const ProtectedLoginRoute = () => {
+  const [memberInfo, setMemberInfo] = useRecoilState(authState);
+
+  useEffect(() => {
+    if (!memberInfo.accessToken) {
+      Swal.fire({
+        title: '로그인해주세요',
+        icon: 'warning',
+        confirmButtonText: '확인',
+      });
+    }
+  }, [memberInfo]);
+
+  return memberInfo.accessToken ? <Outlet /> : <Navigate to="/login" replace />;
+};
 
 export default function Router() {
   const routes = useRoutes([
     {
-      element: (
-        <DashboardLayout>
-          <Suspense>
-            <Outlet />
-          </Suspense>
-        </DashboardLayout>
-      ),
+      element: <ProtectedLoginRoute />,
       children: [
         {
-          path: '/',
-          element: <Navigate to="/campaign/placeTraffic" replace />,
-          index: true,
+          element: (
+            <DashboardLayout>
+              <Suspense fallback={<div>Loading...</div>}>
+                <Outlet />
+              </Suspense>
+            </DashboardLayout>
+          ),
+          children: [
+            {
+              path: '/',
+              element: <Navigate to="/campaign/placeTraffic" replace />,
+              index: true,
+            },
+            { path: 'user', element: <UserPage /> },
+            { path: 'user/new', element: <UserCreatePage /> },
+            { path: 'user/edit/:userId', element: <UserEditPage /> },
+            { path: 'campaign/:type', element: <CampaignPage /> },
+            { path: 'campaign/create', element: <CampaignCreatePage /> },
+            { path: 'campaign/edit/:campaignId', element: <CampaignEditPage /> },
+          ],
         },
-        { path: 'user', element: <UserPage /> },
-        { path: 'user/new', element: <UserCreatePage /> },
-        { path: 'user/edit/:userId', element: <UserEditPage /> },
-        { path: 'campaign/:type', element: <CampaignPage /> },
-        { path: 'campaign/create', element: <CampaignCreatePage /> },
-        { path: 'campaign/edit/:campaignId', element: <CampaignEditPage /> },
       ],
     },
     {
