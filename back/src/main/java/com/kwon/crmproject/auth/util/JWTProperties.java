bdf50a22d468e3b2cd1ae623b72d5a7925665678
권도현 <tobeykwon@gmail.com>
2024-07-08 22:25:04 +0900

    feat: jwtUtil 쿠키 이름 및 만료 시간 getMethod 전략 패턴 구현

--- back/src/main/java/com/kwon/crmproject/auth/util/JWTProperties.java
+++ back/src/main/java/com/kwon/crmproject/auth/util/JWTProperties.java
@@ -9,21 +9,46 @@ import org.springframework.stereotype.Component;
 public class JWTProperties {
     public final String ACCESS_TOKEN_HEADER = "accessToken";
 
+    public final String ACCESS_TOKEN_COOKIE_NAME = "accessToken";
     public final String REFRESH_TOKEN_COOKIE_NAME = "refreshToken";
 
     public final String SECRET_KEY;
 
-    public final long ACCESS_TOKEN_EXPIRATION_TIME;
+    public final long ACCESS_TOKEN_EXPIRATION_SECONDS;
 
-    public final long REFRESH_TOKEN_EXPIRATION_TIME;
+    public final long REFRESH_TOKEN_EXPIRATION_SECONDS;
 
     public JWTProperties(
             @Value("${jwt.secretKey}") String secretKey,
-            @Value("${jwt.accessTokenExpirationTime}") long ACCESS_TOKEN_EXPIRATION_TIME,
-            @Value("${jwt.refreshTokenExpirationTime}") long REFRESH_TOKEN_EXPIRATION_TIME
+            @Value("${jwt.accessTokenExpirationTime}") long ACCESS_TOKEN_EXPIRATION_SECONDS,
+            @Value("${jwt.refreshTokenExpirationTime}") long REFRESH_TOKEN_EXPIRATION_SECONDS
     ) {
         this.SECRET_KEY = secretKey;
-        this.ACCESS_TOKEN_EXPIRATION_TIME = ACCESS_TOKEN_EXPIRATION_TIME;
-        this.REFRESH_TOKEN_EXPIRATION_TIME = REFRESH_TOKEN_EXPIRATION_TIME;
+        this.ACCESS_TOKEN_EXPIRATION_SECONDS = ACCESS_TOKEN_EXPIRATION_SECONDS * 1000L;
+        this.REFRESH_TOKEN_EXPIRATION_SECONDS = REFRESH_TOKEN_EXPIRATION_SECONDS * 1000L;
+    }
+
+    public String getTokenCookieName(TokenType tokenType) {
+        switch (tokenType) {
+            case REFRESH_TOKEN -> {
+                return REFRESH_TOKEN_COOKIE_NAME;
+            }
+            case ACCESS_TOKEN -> {
+                return ACCESS_TOKEN_COOKIE_NAME;
+            }
+        }
+        return null;
+    }
+
+    public long getExpirationSeconds(TokenType tokenType) {
+        switch (tokenType) {
+            case REFRESH_TOKEN -> {
+                return REFRESH_TOKEN_EXPIRATION_SECONDS;
+            }
+            case ACCESS_TOKEN -> {
+                return ACCESS_TOKEN_EXPIRATION_SECONDS;
+            }
+        }
+        return 0;
     }
 }
