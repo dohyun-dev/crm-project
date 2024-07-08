851b16b99f29b1469482dea186bcacfd37c1745d
권도현 <tobeykwon@gmail.com>
2024-07-08 22:33:33 +0900

    feat: 프론트 reissue API 추가

--- front/src/apis/auth.js
+++ front/src/apis/auth.js
@@ -1,13 +1,19 @@
-import axiosInstance from './axios';
 import API_URLS from '../constants/urls';
+import axiosInstance, { axiosInstance2 } from './axios';
 
 const login = async (data) => {
   const response = await axiosInstance.post(API_URLS.AUTH.LOGIN_URL(), data);
   return response.data;
 };
 
+const reissue = async () => {
+  const response = await axiosInstance2.post(API_URLS.AUTH.reissue());
+  return response.data;
+};
+
 const AUTH_API = {
   login,
+  reissue,
 };
 
 export default AUTH_API;
