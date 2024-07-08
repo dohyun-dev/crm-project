96db955517d748eac79f56c65864c7fe7fc1102d
권도현 <tobeykwon@gmail.com>
2024-07-08 22:24:00 +0900

    feat: 백엔드 MemberRepository findByNameContaining 추가

--- back/src/main/java/com/kwon/crmproject/member/domain/repository/MemberRepository.java
+++ back/src/main/java/com/kwon/crmproject/member/domain/repository/MemberRepository.java
@@ -2,12 +2,12 @@ package com.kwon.crmproject.member.domain.repository;
 
 import com.kwon.crmproject.member.domain.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByName(String name);
+    Optional<Member> findByNameContaining(String name);
     Optional<Member> findByUsername(String username);
     boolean existsByUsername(String username);
     boolean existsByName(String name);
