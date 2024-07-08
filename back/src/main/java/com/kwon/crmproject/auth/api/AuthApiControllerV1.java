d2b77a566aeb1c0ac7fc2b54d716f8843c9e0ac2
권도현 <tobeykwon@gmail.com>
2024-07-08 22:27:57 +0900

    refact: AuthControllerV1 리팩토링

--- back/src/main/java/com/kwon/crmproject/auth/api/AuthApiControllerV1.java
+++ back/src/main/java/com/kwon/crmproject/auth/api/AuthApiControllerV1.java
@@ -5,57 +5,62 @@ import com.kwon.crmproject.auth.dto.AuthResponse;
 import com.kwon.crmproject.auth.dto.AuthServiceDto;
 import com.kwon.crmproject.auth.service.AuthServiceV1;
 import com.kwon.crmproject.auth.util.JWTProperties;
-import com.kwon.crmproject.common.exception.CustomException;
-import com.kwon.crmproject.common.exception.ErrorType;
+import com.kwon.crmproject.auth.util.JWTUtil;
+import com.kwon.crmproject.auth.util.TokenType;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
-import java.util.Optional;
-
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/auth")
-public class AuthApiController {
+public class AuthApiControllerV1 {
 
     private final AuthServiceV1 authService;
     private final JWTProperties jwtProperties;
+    private final JWTUtil jwtUtil;
 
     @PostMapping("/login")
     public AuthResponse.TokenDto login(@RequestBody AuthRequest.Login request, HttpServletResponse response) {
         AuthServiceDto.Login loginDto = authService.login(request.getUsername(), request.getPassword());
-        setRefreshTokenInCookies(loginDto.tokenDto().refreshToken(), response);
+        setTokenInCookies(loginDto.tokenDto(), response);
         return new AuthResponse.TokenDto(loginDto.tokenDto().accessToken());
     }
 
     @PostMapping("/reissue")
     public AuthResponse.TokenDto reissue(HttpServletRequest request, HttpServletResponse response) {
-        Optional<Cookie> optionalRefreshTokenCookie = Arrays.stream(request.getCookies())
-                .filter(c -> c.getName().equals(jwtProperties.REFRESH_TOKEN_COOKIE_NAME))
-                .findFirst();
-
-        if (!optionalRefreshTokenCookie.isPresent())
-            throw CustomException.of(ErrorType.TOKEN_EXPIRED);
-
-        AuthServiceDto.TokenDto tokenDto = authService.reissue(optionalRefreshTokenCookie.get().getValue());
-
-        setRefreshTokenInCookies(tokenDto.refreshToken(), response);
-
+        String refreshToken = jwtUtil.findTokenInCookies(TokenType.REFRESH_TOKEN, request);
+        AuthServiceDto.TokenDto tokenDto = authService.reissue(refreshToken);
+        setTokenInCookies(tokenDto, response);
         return new AuthResponse.TokenDto(tokenDto.accessToken());
     }
 
-    private void setRefreshTokenInCookies(String refreshToken, HttpServletResponse response) {
-        Cookie refreshTokenCookie = new Cookie(jwtProperties.REFRESH_TOKEN_COOKIE_NAME, refreshToken);
-        // refreshTokenCookie.setSecure(true);
-        refreshTokenCookie.setHttpOnly(true);
-        refreshTokenCookie.setPath("/");
-        refreshTokenCookie.setMaxAge((int) jwtProperties.REFRESH_TOKEN_EXPIRATION_TIME / 1000);
-        response.addCookie(refreshTokenCookie);
+    private void setTokenInCookies(AuthServiceDto.TokenDto tokenDto, HttpServletResponse response) {
+        setCookies(
+                TokenType.ACCESS_TOKEN,
+                tokenDto.accessToken(),
+                response
+        );
+
+        setCookies(
+                TokenType.REFRESH_TOKEN,
+                tokenDto.refreshToken(),
+                response
+        );
+    }
+
+    private void setCookies(TokenType tokenType, String value, HttpServletResponse response) {
+        Cookie tokenCookie = new Cookie(jwtProperties.getTokenCookieName(tokenType), value);
+        tokenCookie.setHttpOnly(true);
+        tokenCookie.setPath("/");
+        tokenCookie.setMaxAge((int) (jwtProperties.getExpirationSeconds(tokenType)));
+        response.addCookie(tokenCookie);
     }
 }
