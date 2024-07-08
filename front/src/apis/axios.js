ca401eeee8266f9952783704fc62ab32bd809ab3
권도현 <tobeykwon@gmail.com>
2024-07-08 22:45:16 +0900

    fix: 프론트 axios 인터셉터 수정

--- front/src/apis/axios.js
+++ front/src/apis/axios.js
@@ -37,24 +37,22 @@ export const useAxiosInterceptors = () => {
       if (error.response.status === 403 && !originalRequest._retry) {
         originalRequest._retry = true;
 
-        const response = await API.AUTH_API.reissue();
-        setMemberInfo((prevState) => ({
-          accessToken: response.data.accessToken,
-          ...decodeJWTWithoutVerification(response.data.accessToken),
-        }));
-        return axiosInstance(originalRequest);
-      }
-      if (error) {
-        navigate('/login');
-        Swal.fire({
-          title: '실패!',
-          text:
-            error.response.status === 403
-              ? '접근 권한이 없습니다. 다시 로그인 해주세요'
-              : '로그인 정보가 만료되었습니다. 다시 로그인해주세요.',
-          icon: 'error',
-          confirmButtonText: '확인',
-        });
+        try {
+          const response = await API.AUTH_API.reissue();
+          setMemberInfo((prevState) => ({
+            accessToken: response.data.accessToken,
+            ...decodeJWTWithoutVerification(response.data.accessToken),
+          }));
+          return axiosInstance(originalRequest);
+        } catch (e) {
+          navigate('/login');
+          Swal.fire({
+            title: '실패!',
+            text: '접근 권한이 없습니다. 다시 로그인 해주세요',
+            icon: 'error',
+            confirmButtonText: '확인',
+          });
+        }
       }
     }
   );
