506cab3b9c15817cebc527fb5133e82e5b874768
권도현 <tobeykwon@gmail.com>
2024-07-09 23:00:10 +0900

    build: 배포 설정 추가

--- back/src/main/java/com/kwon/crmproject/CrmProjectApplication.java
+++ back/src/main/java/com/kwon/crmproject/CrmProjectApplication.java
@@ -2,14 +2,20 @@ package com.kwon.crmproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
-public class CrmProjectApplication {
+public class CrmProjectApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(CrmProjectApplication.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(CrmProjectApplication.class);
+    }
 }
---
+++ back/src/main/java/com/kwon/crmproject/common/config/CreateAdminConfig.java
@@ -0,0 +1,26 @@
+package com.kwon.crmproject.common.config;
+
+import com.kwon.crmproject.member.domain.entity.Member;
+import com.kwon.crmproject.member.domain.repository.MemberRepository;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@RequiredArgsConstructor
+public class CreateAdminConfig {
+
+    private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @PostConstruct
+    public void init() {
+        Member admin = Member.builder()
+                .name("관리자")
+                .username("admin")
+                .password(passwordEncoder.encode("admin1234"))
+                .build();
+        memberRepository.save(admin);
+    }
+}
